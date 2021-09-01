b4a2b0884ac8ee8f60f899880afc685797db6d4d
Blink Tenor <blinktenor@gmail.com>
2021-09-01 11:51:12 -0500

    Add cors

--- src/CardAPIService.js
+++ src/CardAPIService.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import archidekt from 'archidekt';
 
+const headers = {"Access-Control-Allow-Origin": "*"};
+
 const fetchDecklistFromFolder = async (folderId) => {
-  const response = await axios.get(`https://www.archidekt.com/api/decks/folders/${folderId}/?dir=asc&orderBy=name`);
+  const response = await axios.get(
+    `https://www.archidekt.com/api/decks/folders/${folderId}/?dir=asc&orderBy=name`,
+    headers,
+  );
   const { decks } = response.data;
   return decks;
 };
@@ -14,7 +19,10 @@ const fetchCardlistFromDeck = async (deckId) => {
 };
 
 const fetchPriceFromScryfall = async (cardName) => {
-	const response = await axios.get(`https://api.scryfall.com/cards/search?q=${cardName} cheapest:usd`);
+	const response = await axios.get(
+    `https://api.scryfall.com/cards/search?q=${cardName} cheapest:usd`,
+    headers,
+  );
   return response.data.data[0].prices.usd;
 }
 
