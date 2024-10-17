af599e54e9b70b68e80bfc89565935b822964508
Blink Tenor <blinktenor@gmail.com>
2024-10-17 15:14:12 -0500

    Wrap in try catch

--- src/CardAPIService.js
+++ src/CardAPIService.js
@@ -27,9 +27,14 @@ const fetchPriceFromScryfall = async (cardName) => {
   if (scryfallCards[cardName]) {
     return scryfallCards[cardName].prices.usd;
   } else {
-    const response = await axios.get(`https://api.scryfall.com/cards/search?q=${cardName} cheapest:usd`);
-    scryfallCards[cardName] = response.data.data[0];
-    return response.data.data[0].prices.usd;
+    try {
+      const response = await axios.get(`https://api.scryfall.com/cards/search?q=${cardName} cheapest:usd`);
+      scryfallCards[cardName] = response.data.data[0];
+      return response.data.data[0].prices.usd;
+    }
+    catch (e) {
+      console.log(e);
+    }
   }
 }
 
@@ -37,10 +42,15 @@ const fetchRarityFromScryfall = async (cardName) => {
   if (scryfallCards[cardName]) {
     return scryfallCards[cardName].rarity;
   } else {
-    const response = await axios.get(`https://api.scryfall.com/cards/search?q=${cardName}+unique%3Aprints&order=released`);
-    const matches = response.data.data.filter((card) => card.name === cardName);
-    scryfallCards[cardName] = matches[0];
-    return matches[0].rarity;
+    try {
+      const response = await axios.get(`https://api.scryfall.com/cards/search?q=${cardName}+unique%3Aprints&order=released`);
+      const matches = response.data.data.filter((card) => card.name === cardName);
+      scryfallCards[cardName] = matches[0];
+      return matches[0].rarity;
+    }
+    catch (e) {
+      console.log(e);
+    }
   }
 }
 
